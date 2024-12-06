baf984b165a6cb3d0e1bf9b7cedbd4733c88a729
thejahid <thejahid@icloud.com>
2024-12-06 22:25:11 +0600

    [SFT-1635]: insert category names into the browser title

--- src/theme/DocItem/Metadata/index.tsx
+++ src/theme/DocItem/Metadata/index.tsx
@@ -1,6 +1,9 @@
 import React, { useMemo } from 'react';
 import { PageMetadata } from '@docusaurus/theme-common';
-import { useDoc } from '@docusaurus/plugin-content-docs/client';
+import {
+  useDoc,
+  useSidebarBreadcrumbs,
+} from '@docusaurus/plugin-content-docs/client';
 import { useLocation } from '@docusaurus/router';
 
 import allPlugins from '@site/allPlugins.json';
@@ -8,34 +11,52 @@ import allPlugins from '@site/allPlugins.json';
 export default function DocItemMetadata(): JSX.Element {
   const { metadata, frontMatter, assets } = useDoc();
   const { pathname } = useLocation();
+  const breadcrumbs = useSidebarBreadcrumbs();
 
   const mappedTitle = useMemo(() => {
     const matchingPlugin = allPlugins.find((plugin) =>
       plugin.items.some((item) => pathname.startsWith(item.slug))
     );
-
     if (!matchingPlugin) return metadata.title;
 
     const matchingItem = matchingPlugin.items.find((item) =>
       pathname.startsWith(item.slug)
     );
-
     if (!matchingItem) return metadata.title;
 
+    const breadcrumbLabel =
+      breadcrumbs.length > 0 &&
+      breadcrumbs[0].type === 'category' &&
+      metadata.title !== breadcrumbs[0].label
+        ? `${metadata.title} - ${breadcrumbs[0].label}`
+        : metadata.title;
+
     if (pathname.includes('/classic/')) {
-      return `${metadata.title} | ${matchingItem.name} Classic for ${matchingPlugin.name}`;
+      return `${breadcrumbLabel} | ${matchingItem.name} Classic for ${matchingPlugin.name}`;
     }
 
     const versionMatch = pathname.match(/\/v(\d+)/);
     const version = versionMatch ? `${versionMatch[1]}.x` : '';
 
-    return `${metadata.title} | ${matchingItem.name} ${version} for ${matchingPlugin.name}`;
-  }, [pathname, metadata.title]);
+    return `${breadcrumbLabel} | ${matchingItem.name} ${version} for ${matchingPlugin.name}`;
+  }, [pathname, metadata.title, breadcrumbs]);
+
+  const mappedDescription = useMemo(() => {
+    const matchingPlugin = allPlugins.find((plugin) =>
+      plugin.items.some((item) => pathname.startsWith(item.slug))
+    );
+
+    const matchingItem = matchingPlugin?.items.find((item) =>
+      pathname.startsWith(item.slug)
+    );
+
+    return matchingItem?.metaDescription;
+  }, [metadata.description, pathname]);
 
   return (
     <PageMetadata
       title={mappedTitle}
-      description={metadata.description}
+      description={mappedDescription}
       keywords={frontMatter.keywords}
       image={assets.image ?? frontMatter.image}
     />
